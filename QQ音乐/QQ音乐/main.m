3b8d1f9c2aeb466a2a4502fd15ebab7f855223ed
Rookeen <734134823@qq.com>
2015-12-11 19:37:12 +0800

    简易音乐播放器Demo
    模仿QQ音乐界面

---
+++ QQ音乐/QQ音乐/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  QQ音乐
+//
+//  Created by Rockeen on 15/12/10.
+//  Copyright © 2015年 https://github.com/rockeen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
